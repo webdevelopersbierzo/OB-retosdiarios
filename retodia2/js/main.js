9ad00eb4e11226063b11de8c19e43640f9f5cf44
Oscar Corral Garcia <oscarcorralg@hotmail.com>
2021-11-09 13:47:29 +0100

    finalizada javascript funcion calculator

--- retodia2/js/main.js
+++ retodia2/js/main.js
@@ -1,8 +1,8 @@
 // formulario
 // Hacer visible el valor del slider monthly_fees
 
-var monthly_fees =document.getElementById("monthly_fees");
-var output_monthly = document.getElementById("monthly_range");
+let monthly_fees =document.getElementById("monthly_fees");
+let output_monthly = document.getElementById("monthly_range");
 output_monthly.innerHTML = monthly_fees.value; 
 
 monthly_fees.oninput =function(){
@@ -19,13 +19,37 @@ interests.oninput = function(){
 // Calculadora
 
 function calculator(){
-    let debt = (document.querySelector("input[name=amount]").value);
-    let monthly = (document.querySelector("input[name= monthly_fees]").value);
-    let interests= (document.querySelector("input[name=interests]").value);
+
+    // variables
+
+    let debt = parseFloat(document.querySelector("input[name=amount]").value);
+    let monthly = parseFloat(document.querySelector("input[name= monthly_fees]").value);
+    let interests= parseFloat (document.querySelector("input[name=interests]").value);
     const result = document.getElementById("result");
 
     // calculos
+    interests = interests/100;
+    let interests_year = (debt * interests);
+    debt += interests_year;
+    console.log(debt);// BORRAR
+    let cuota = (debt)/monthly; 
 
-    //TODO
-}
+    // añadimos los resultados a id="result"
+    document.getElementById("capital_total").innerHTML = debt;
+    document.getElementById("interest_type").innerHTML = interests;
+    document.getElementById("monthly_dues").innerHTML = monthly;
+    document.getElementById("dues").innerHTML = cuota ;
+
+    
+    /*
+    let j=1;
+    for(let i = 0; i <= (monthly-1) ; i++){
+        console.log ( j +" cuota : " +  cuota + "€");// BORRAR
+        debt = debt - cuota;
+        console.log(debt);// BORRAR
+        j++
+    }*/
+}    
+ 
+
 
